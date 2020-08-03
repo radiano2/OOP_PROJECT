d4430b6a8f55c3c09656a133b13313fb51832fee
radiano2 <57500739+radiano2@users.noreply.github.com>
2020-08-03 11:07:52 +0300

    MEME2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rectangle perimeter_object = new Rectangle(2, 3);
-        System.out.println(perimeter_object.perimeter());
+//        Rectangle perimeter_object = new Rectangle(0,0);
+//        System.out.println(perimeter_object.perimeter(2 , 3));
 
         Circle circle_formula = new Circle(5 );
         circle_formula.area(5);
--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -5,15 +5,15 @@ public class Rectangle {
     int height ;
     int width ;
 
-    public Rectangle() {
-        System.out.println("Cretingng");
-    }
+//    public Rectangle() {
+//        System.out.println("Cretingng");
+//    }
 
     public Rectangle(int height , int width) {
         System.out.println("Cretingng");
     }
 
-    public int perimeter(){
+    public int perimeter(int height , int width){
         return 2*height + 2*width;
     }
 
